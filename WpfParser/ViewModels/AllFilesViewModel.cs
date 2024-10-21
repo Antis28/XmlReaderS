9f8efaea1757adb3822b44f2a822f9166e0e3491
Antis <antis28@mail.ru>
2024-10-22 01:20:14 +0300

    Сортировка имеи файла

--- WpfParser/ViewModels/AllFilesViewModel.cs
+++ WpfParser/ViewModels/AllFilesViewModel.cs
@@ -18,16 +18,11 @@ namespace WpfParser.ViewModels
 {
     internal class AllFilesViewModel : ViewModel
     {
-        
-        #region IsOnlyDck : bool - Отображать только Dck
-        ///<summary>Отображать только Dck</summary>
+        ///<summary>Список Отчет По Получателю</summary>
         private IEnumerable<ResponseFileViewModel> _ResponseFiles ;
-        ///<summary>Отображать только Dck</summary>
+        ///<summary>Список Отчет По Получателю</summary>
         public IEnumerable<ResponseFileViewModel> ResponseFiles { get => _ResponseFiles; set => Set(ref _ResponseFiles, value); }
-        #endregion
-
-
-
+       
 
         #region SelectedFile : ResponseFileViewModel - Выбранный файл xml
         ///<summary>Выбранная группа</summary>
@@ -38,9 +33,23 @@ namespace WpfParser.ViewModels
 
         #region IsOnlyDck : bool - Отображать только Dck
         ///<summary>Отображать только Dck</summary>
-        private bool _IsOnlyDck = true;
+        private bool _IsOnlyDck = false;
         ///<summary>Отображать только Dck</summary>
-        public bool IsOnlyDck { get => _IsOnlyDck; set => Set(ref _IsOnlyDck, value); } 
+        public bool IsOnlyDck { get => _IsOnlyDck; set => Set(ref _IsOnlyDck, value); }
+        #endregion
+
+        #region IsOnlyDis : bool - Отображать только Dis
+        ///<summary>Отображать только Dck</summary>
+        private bool _IsOnlyDis = true;
+        ///<summary>Отображать только Dck</summary>
+        public bool IsOnlyDis { get => _IsOnlyDis; set => Set(ref _IsOnlyDis, value); }
+        #endregion
+
+        #region IsAllVisible : bool - Отображать только все
+        ///<summary>Отображать только все</summary>
+        private bool _IsAllVisible = false;
+        ///<summary>Отображать только все</summary>
+        public bool IsAllVisible { get => _IsAllVisible; set => Set(ref _IsAllVisible, value); }
         #endregion
 
         #region Commands
@@ -52,11 +61,63 @@ namespace WpfParser.ViewModels
 
         private void OnOnlyDckCommandExecuted(object p)
         {
-            OnlyDckFiltred();
+            CheckVisibleFileName();
         }
 
-        private void OnlyDckFiltred()
+        #endregion
+
+        #region OnlyDisCommand
+
+        public ICommand OnlyDisCommand { get; }
+        private bool CanOnlyDisCommandExecute(object p) => true;
+
+        private void OnOnlyDisCommandExecuted(object p)
         {
+            CheckVisibleFileName();
+        }
+
+        
+
+
+        #endregion
+
+        #region AllVisibleCommand
+
+        public ICommand AllVisibleCommand { get; }
+        private bool CanAllVisibleCommandExecute(object p) => true;
+
+        private void OnAllVisibleCommandExecuted(object p)
+        {
+            CheckVisibleFileName();
+        }
+        #endregion
+
+        #region CheckVisibleFileName
+
+        public ICommand CheckVisibleFileNameCommand { get; }
+        private bool CanCheckVisibleFileNameCommandExecute(object p) => true;
+
+        private void OnCheckVisibleFileNameCommandExecuted(object p)
+        {
+            CheckVisibleFileName();
+        }
+        #endregion
+
+        private void CheckVisibleFileName()
+        {
+            var rf = DataService.ReadResponseFiles();
+            ResponseFiles = new ObservableCollection<ResponseFileViewModel>(rf);
+            
+            if (IsOnlyDis)
+            {
+                foreach (var file in ResponseFiles)
+                {
+                    var indEnd = file.FileName.IndexOf("-DCK-", StringComparison.InvariantCultureIgnoreCase);
+                    file.FileName = file.FileName.Remove(indEnd);
+                    var indStart = file.FileName.IndexOf("-DIS-", StringComparison.InvariantCultureIgnoreCase);
+                    file.FileName = file.FileName.Remove(0, indStart +5);
+                }
+            }
             if (IsOnlyDck)
             {
                 foreach (var file in ResponseFiles)
@@ -64,17 +125,13 @@ namespace WpfParser.ViewModels
                     var indEnd = file.FileName.IndexOf("-001-DOC-", StringComparison.InvariantCultureIgnoreCase);
                     file.FileName = file.FileName.Remove(indEnd);
                     var indStart = file.FileName.IndexOf("-DCK-", StringComparison.InvariantCultureIgnoreCase);
-                    file.FileName = file.FileName.Remove(0, indStart+1);
+                    file.FileName = file.FileName.Remove(0, indStart + 1);
                 }
             }
-            else
-            {
-                var rf = DataService.ReadResponseFiles();
-                ResponseFiles = new ObservableCollection<ResponseFileViewModel>(rf);
-            }
+            ResponseFiles = from item in ResponseFiles
+                                orderby item.FileName
+                                select item;
         }
-
-        #endregion
 
         #endregion
 
@@ -84,13 +141,18 @@ namespace WpfParser.ViewModels
 
             OnlyDckCommand =
                 new LambdaCommand(OnOnlyDckCommandExecuted, CanOnlyDckCommandExecute);
-
-
+            AllVisibleCommand =
+                new LambdaCommand(OnAllVisibleCommandExecuted, CanAllVisibleCommandExecute);
+            OnlyDisCommand
+                =
+                new LambdaCommand(OnOnlyDisCommandExecuted, CanOnlyDisCommandExecute);
+            CheckVisibleFileNameCommand =
+                new LambdaCommand(OnCheckVisibleFileNameCommandExecuted, CanCheckVisibleFileNameCommandExecute);
             #endregion
 
             var rf = DataService.ReadResponseFiles();
             ResponseFiles = new ObservableCollection<ResponseFileViewModel>(rf);
-            OnlyDckFiltred();
+            CheckVisibleFileName();
         }
     }
 }
